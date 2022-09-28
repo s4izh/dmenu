8890c4d44d0eea0678b14e858fe6da616c1d91b6
s4izh <sergiosanz234@gmail.com>
2022-09-28 23:05:08 +0200

    changed dmenu colors and added layout script

--- config.def.h
+++ config.def.h
@@ -4,14 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono NerdFont:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	/* [SchemeSel] = { "#555555", "#AECF96" }, */
+	[SchemeNorm] = { "#a9b1d6", "#24283b" },
+	[SchemeSel] = { "#24283b", "#7da6ff" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
