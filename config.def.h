76627a570bf29352441e333674b24108626eeb28
s4izh <sergiosanz234@gmail.com>
2022-09-29 22:09:30 +0200

    tokyonight theme

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#a9b1d6", "#24283b" },
-	[SchemeSel] = { "#24283b", "#7da6ff" },
+	[SchemeSel] = { "#24283b", "#a9b1d6" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
